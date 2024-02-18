c0f2db0ea960d330ec4515268d051ae6cd09c277
beng2004 <btguerrieri@gmail.com>
2024-02-18 06:12:30 -0500

    updates

--- SingleHandActions.py
+++ SingleHandActions.py
@@ -7,6 +7,8 @@ import cv2
 import mediapipe as mp
 import re
 import commands as c
+import winsound 
+import time
 
 valid_signs = ['Closed_Fist', 'Victory', 'Pointing_Up', 'Thumb_Up']
 valid_browswers = ['Google Chrome', 'Floorp', 'Opera', 'Firefox', 'Microsoft Edge']
@@ -21,7 +23,6 @@ GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 
-video = cv2.VideoCapture(0)
 
 def determine_action(gesture, direction, handedness):
     print(gesture,direction,handedness)
@@ -31,7 +32,7 @@ def determine_action(gesture, direction, handedness):
                 c.open_application('', True)
             elif direction == 'Down':
                 c.take_screenshot()
-                c.describe_image()
+                # c.describe_image()
             elif direction == 'Left':
                 print('Action_C')
             elif direction == 'Right':
@@ -83,9 +84,9 @@ def determine_action(gesture, direction, handedness):
                     c.browser_command('new_tab')
                 elif direction == 'Down':
                     c.browser_command('close_tab')
-                elif direction == 'Left':
-                    c.browser_command('left_tab')
                 elif direction == 'Right':
+                    c.browser_command('left_tab')
+                elif direction == 'Left':
                     c.browser_command('right_tab')
                 else:
                     print('Invalid direction')
@@ -106,11 +107,12 @@ def determine_action(gesture, direction, handedness):
             if direction == 'Up':
                 print('Action_Y')
             elif direction == 'Down':
-                print('Action_Z')
+                c.browser_command('enter')
             elif direction == 'Left':
+                c.browser_command('shift_tab')
                 print('Action_1')
             elif direction == 'Right':
-                print('Action_2')
+                c.browser_command('tab')
             else:
                 print('Invalid direction')
         else:
@@ -121,6 +123,7 @@ def determine_action(gesture, direction, handedness):
             elif direction == 'Left':
                 print('Action_5')
             elif direction == 'Right':
+
                 print('Action_6')
             else:
                 print('Invalid direction')
@@ -144,8 +147,8 @@ options = GestureRecognizerOptions(
      
 #takes input from camera 
 cap = cv2.VideoCapture(0)
-cv2.namedWindow("Window", cv2.WND_PROP_AUTOSIZE)
-cv2.setWindowProperty("Window", cv2.WND_PROP_AUTOSIZE, cv2.WINDOW_AUTOSIZE)
+# cv2.namedWindow("Window", cv2.WND_PROP_AUTOSIZE)
+# cv2.setWindowProperty("Window", cv2.WND_PROP_AUTOSIZE, cv2.WINDOW_AUTOSIZE)
 
 #mediapipe functions
 mpDraw = mp.solutions.drawing_utils
@@ -160,14 +163,14 @@ majority_guess = 'None'
 with GestureRecognizer.create_from_options(options) as recognizer, mp_hands.Hands(
     static_image_mode=True,
     max_num_hands=1,
-    min_detection_confidence=0.5) as hands:
+    min_detection_confidence=0.81) as hands:
     avg_handedness = [] 
     while True: #need a while True loop because program is continuosuly grabbing input from camera until user ends the program
         if len(avg_handedness) >= 100:
             avg_handedness = []
         #array to save each hand's landmarks
-        prev_x = x_cords
-        prev_y = y_cords
+        # prev_x = x_cords
+        # prev_y = y_cords
         x_cords = []
         y_cords = []
         
@@ -202,7 +205,8 @@ with GestureRecognizer.create_from_options(options) as recognizer, mp_hands.Hand
                 for landmark in eachHand.landmark:
                     x_cords.append(landmark.x)
                     y_cords.append(landmark.y)
-
+            prev_x = x_cords
+            prev_y = y_cords
             # if any(x < 0 or x > 1 or y < 0 or y > 1 for x, y in zip(x_cords, y_cords)):
             #     print("Hand outside the screen!")
             
@@ -212,9 +216,17 @@ with GestureRecognizer.create_from_options(options) as recognizer, mp_hands.Hand
 
             if current_gesture_label in valid_signs:
                 last_valid_sign = current_gesture_label
+            # if majority_guess in valid_signs:
+            #     last_valid_sign = majority_guess
 
             majority_guess = max(set(last_guesses), key=last_guesses.count)
+            leftmost_x = min(x_cords)
+            rightmost_x = max(x_cords)
+            topmost_y = min(y_cords)
+            bottommost_y = max(y_cords)
 
+            # avg_x= (leftmost_x + rightmost_x) / 2
+            # avg_y = (topmost_y + bottommost_y) / 2
             avg_x = np.mean(x_cords)
             avg_y = np.mean(y_cords)
             hand = max(set(avg_handedness), key = avg_handedness.count)
@@ -241,18 +253,32 @@ with GestureRecognizer.create_from_options(options) as recognizer, mp_hands.Hand
 
             if .4 <= avg_x <= .6 and .4 <= avg_y <= .6 and accepting_gesture == False:
                 accepting_gesture = True
-                avg_handedness = []
+                # avg_handedness = []
+                last_guesses = []
+                winsound.Beep(1000, 200)
                 print('centered')
         else:
             if accepting_gesture and last_valid_sign is not None:
+                # avg_x = np.mean(prev_x)
+                # avg_y = np.mean(prev_y)
                 leftmost_x = min(prev_x)
                 rightmost_x = max(prev_x)
                 topmost_y = min(prev_y)
                 bottommost_y = max(prev_y)
+                # min_value = min(leftmost_x, rightmost_x, topmost_y, bottommost_y)
 
+                # if min_value == rightmost_x:
+                #     print('Left')
+                # elif min_value == leftmost_x:
+                #     print('Right')
+                # elif min_value == topmost_y:
+                #     print('Top')
+                # else:
+                #     print('Bottom')
+                # accepting_gesture = False
                 x = (leftmost_x + rightmost_x) / 2
                 y = (topmost_y + bottommost_y) / 2
-
+                
                 if not (.4 <= x <= .6 and .4 <= y <= .6):
                     if x < .5:
                         distance_x = x
@@ -303,7 +329,7 @@ with GestureRecognizer.create_from_options(options) as recognizer, mp_hands.Hand
                     accepting_gesture = False
             
 
-        cv2.imshow('Window', frame)
+        # cv2.imshow('Window', frame)
         
         #esc key will end the program
         if cv2.waitKey(5) & 0xFF == 27:
